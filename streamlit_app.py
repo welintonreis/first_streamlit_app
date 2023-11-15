1f2baa2c9dcdd5357c4af1525d5ccfd771c16e51
Welinton dos Reis Gonçalves <wrgoncalves@santillana.com>
2023-11-15 16:46:56 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,4 @@ my_data_rows = my_cur.fetchall()
 
 fruit_add = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('The user entered ', fruit_add)
-my_cur.execute(f"insert into pc_rivery_db.public.fruit_load_list values (fruit_add)")
+my_cur.execute(f"insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
