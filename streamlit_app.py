350ceaa5a307964f6bb590f27a055243e68cccba
Welinton dos Reis Gonçalves <wrgoncalves@santillana.com>
2023-11-15 16:52:12 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
 
 streamlit.title("My Mom's New Health Dinner")
 streamlit.header('Breakfast Menu')
@@ -44,8 +43,8 @@ streamlit.dataframe(fruityvice_normalized)
 #streamlit.text("Hello from Snowflake:")
 #streamlit.text(my_data_row)
 
-my_cur.execute("select * from fruit_load_list")
 streamlit.header("The fruit load list contains:")
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 
 fruit_add = streamlit.text_input('What fruit would you like to add?','Kiwi')
