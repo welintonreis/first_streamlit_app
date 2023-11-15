ef3b35ebb5014de778c4a2761ff255244c3936d4
Welinton dos Reis Gonçalves <wrgoncalves@santillana.com>
2023-11-15 16:35:20 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contents")
